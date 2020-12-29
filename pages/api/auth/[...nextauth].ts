f1122e0cc378dd6a91e897a6aef270a009f12ce9
Yu MORIYA <euro21st@gmail.com>
2020-12-29 14:30:48 +0900

    setup next-auth and prisma

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import { PrismaClient } from "@prisma/client";
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+};
+export default (req, res) => NextAuth(req, res, options);
