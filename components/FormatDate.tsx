4f246ff8d171271a0d71677d0217650d70a25030
Yu MORIYA <euro21st@gmail.com>
2020-12-30 16:24:20 +0900

    Remove unused import

--- components/FormatDate.tsx
+++ components/FormatDate.tsx
@@ -1,4 +1,4 @@
-import { format, isDate } from 'date-fns'
+import { format } from 'date-fns'
 
 const FormatDate = ({ date }: { date: Date }) => {
   return <time dateTime={date.toString()}>{format(date, 'yyyy/MM/dd HH:mm:ss')}</time>
