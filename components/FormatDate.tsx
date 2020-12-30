a5037dadd02612d877a52a36b73451ee3faf8f93
Yu MORIYA <euro21st@gmail.com>
2020-12-30 16:32:38 +0900

    Fix datetime bug

--- components/FormatDate.tsx
+++ components/FormatDate.tsx
@@ -1,7 +1,10 @@
 import { format } from 'date-fns'
+import { utcToZonedTime } from 'date-fns-tz'
+import ja from 'date-fns/locale/ja'
 
 const FormatDate = ({ date }: { date: Date }) => {
-  return <time dateTime={date.toString()}>{format(date, 'yyyy/MM/dd HH:mm:ss')}</time>
+  const jstDate = utcToZonedTime(date, 'Asia/Tokyo')
+  return <time dateTime={jstDate.toString()}>{format(jstDate, 'yyyy/MM/dd HH:mm:ss', { locale: ja})}</time>
 }
 
 export default FormatDate
